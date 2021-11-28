e0e298a61ffbf7f8454b9caafaa1fbd42bffb4c6
adam <adam.lepkowski@hotmail.com>
2021-11-28 15:23:27 +0100

    Add _configure_colums method

--- registration_frame.py
+++ registration_frame.py
@@ -8,7 +8,6 @@ class RegistrationFrame(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
-        self.columnconfigure(0, weight=1)
         self.lbl_pat_details = tk.Label(self, text='Patient Details')
         self.lbl_pat_details.grid(row=0, column=0, sticky='we', columnspan=2)
         self.lbl_f_name = tk.Label(self, text='First Name')
@@ -17,3 +16,9 @@ class RegistrationFrame(tk.Frame):
         self.ent_f_name.grid(row=1, column=1, sticky='w')
         self.btn_register = tk.Button(self, text='Register')
         self.btn_register.grid(row=2, column=0)
+        self._configure_columns()
+
+    def _configure_columns(self):
+        columns, rows = self.grid_size()
+        for column in range(columns):
+            self.columnconfigure(column, weight=1)
