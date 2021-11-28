6550f992fa0e45aa8c11b56d25d0b7a0f43d36d0
adam <adam.lepkowski@hotmail.com>
2021-11-28 18:43:08 +0100

    Add _get_columns_patient method

--- db.py
+++ db.py
@@ -16,6 +16,12 @@ class DB:
         self.cur.execute(CREATE_TABLE_PATIENT)
         self.cur.connection.commit()
 
+    def _get_columns_patient(self):
+        sql = "PRAGMA table_info('patient')"
+        columns = self.cur.execute(sql).fetchall()
+        column_names = [column[1] for column in columns]
+        return column_names
+
     def register_patient(self, first_name):
         sql = "INSERT INTO patient (first_name) VALUES (?)"
         self.cur.execute(sql, (first_name,))
