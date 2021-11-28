0ab970ca6e0fc4be3b00621c3f047a62a76a300f
adam <adam.lepkowski@hotmail.com>
2021-11-28 13:51:32 +0100

    Create table patient

--- db.py
+++ db.py
@@ -1,9 +1,17 @@
 import sqlite3
 
 
+CREATE_TABLE_PATIENT = """
+    CREATE TABLE IF NOT EXISTS patient (
+        id          INTEGER PRIMARY KEY,
+        first_name  TEXT NOT NULL
+    )
+"""
+
 class DB:
 
     def __init__(self, db):
         self.con = sqlite3.connect(db)
         self.cur = self.con.cursor()
-        
+        self.cur.execute(CREATE_TABLE_PATIENT)
+        self.cur.connection.commit()
