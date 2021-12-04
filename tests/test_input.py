4e3d735ef51623b18b4c40058d4dc3ef2bba2d66
adam <adam.lepkowski@hotmail.com>
2021-12-04 12:38:36 +0100

    Add gender input

--- tests/test_input.py
+++ tests/test_input.py
@@ -3,5 +3,6 @@ PATIENT_INPUT_1 = {
     'first_name': 'First',
     'middle_name': 'Middle',
     'last_name': 'Last',
-    'date_of_birth': '1900-10-10'
+    'date_of_birth': '1900-10-10',
+    'gender': 'Male'
 }
