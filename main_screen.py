4edac182692c77c14819deffa1841b33048b0a0d
adam <adam.lepkowski@hotmail.com>
2021-12-06 14:48:26 +0100

    Add title and registration button

--- main_screen.py
+++ main_screen.py
@@ -5,3 +5,9 @@ class MainScreen(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
+        self.lbl_title = tk.Label(self, text='Clinic Management System')
+        self.lbl_title.grid(row=0, column=0, sticky='we')
+        self.frm_buttons = tk.Frame(self)
+        self.frm_buttons.grid(row=1, column=0, sticky='nsew')
+        self.btn_reg = tk.Button(self.frm_buttons, text='Register')
+        self.btn_reg.grid(row=0, column=0)
