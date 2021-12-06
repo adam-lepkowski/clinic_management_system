116c2f0a77489934618f22f062e60ff840e7c602
adam <adam.lepkowski@hotmail.com>
2021-12-06 19:35:11 +0100

    Add find button

--- title_screen.py
+++ title_screen.py
@@ -9,10 +9,15 @@ class TitleScreen(tk.Frame):
         self.lbl_title = tk.Label(self, text='Clinic Management System')
         self.lbl_title.grid(row=0, column=0, sticky='we', pady=10)
         self.img_reg = tk.PhotoImage(file=r'images\resized\register.png')
+        self.img_find = tk.PhotoImage(file=r'images\resized\find.png')
         self.frm_buttons = tk.Frame(self)
         self.frm_buttons.grid(row=1, column=0, sticky='nsew')
         self.btn_reg = tk.Button(
             self.frm_buttons, text='Register', image=self.img_reg,
             compound=tk.TOP, command=lambda: master.change_frame(1))
         self.btn_reg.grid(row=0, column=0)
+        self.btn_find = tk.Button(
+            self.frm_buttons, text='Find', image=self.img_find, compound=tk.TOP
+        )
+        self.btn_find.grid(row=0, column=1)
         self.grid(row=0, column=1, sticky='nsew')
