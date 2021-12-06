a4067430572dc22d6a68077e01377676bb3e6cec
adam <adam.lepkowski@hotmail.com>
2021-12-06 14:53:25 +0100

    Add register button image

--- main_screen.py
+++ main_screen.py
@@ -6,8 +6,11 @@ class MainScreen(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.lbl_title = tk.Label(self, text='Clinic Management System')
-        self.lbl_title.grid(row=0, column=0, sticky='we')
+        self.lbl_title.grid(row=0, column=0, sticky='we', pady=10)
+        self.img_reg = tk.PhotoImage(file=r'images\resized\register.png')
         self.frm_buttons = tk.Frame(self)
         self.frm_buttons.grid(row=1, column=0, sticky='nsew')
-        self.btn_reg = tk.Button(self.frm_buttons, text='Register')
+        self.btn_reg = tk.Button(
+            self.frm_buttons, text='Register', image=self.img_reg,
+            compound=tk.TOP)
         self.btn_reg.grid(row=0, column=0)
