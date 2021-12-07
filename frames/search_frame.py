791b93b9b8fd37e6b02dd8eb5140b04fbc9120fb
adam <adam.lepkowski@hotmail.com>
2021-12-07 13:49:15 +0100

    Add master param and grid placement

--- frames/search_frame.py
+++ frames/search_frame.py
@@ -3,7 +3,8 @@ import tkinter as tk
 
 class SearchFrame(tk.Frame):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, master):
+        super().__init__(master)
         self.lbl_title = tk.Label(self, text='Find Patients')
         self.lbl_title.grid(row=0, column=0, sticky='nsew')
+        self.grid(row=0, column=1, sticky='nsew')
