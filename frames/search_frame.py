40d2df26f2766f7df0fe6bf5a9391379845e2f05
adam <adam.lepkowski@hotmail.com>
2021-12-07 13:57:22 +0100

    Add button frame and find button

--- frames/search_frame.py
+++ frames/search_frame.py
@@ -11,4 +11,8 @@ class SearchFrame(tk.Frame):
         self.lbl_f_name.grid(row=1, column=0, sticky='e')
         self.ent_f_name = tk.Entry(self)
         self.ent_f_name.grid(row=1, column=1, sticky='we')
+        self.frm_buttons = tk.Frame(self)
+        self.frm_buttons.grid(row=2, column=0, sticky='we')
+        self.btn_find = tk.Button(self.frm_buttons, text='Find')
+        self.btn_find.grid(row=0, column=0)
         self.grid(row=0, column=1, sticky='nsew')
