213c639f65aab99e698430837004570876e934a5
adam <adam.lepkowski@hotmail.com>
2021-12-07 13:53:33 +0100

    Add first_name lbl and ent

--- frames/search_frame.py
+++ frames/search_frame.py
@@ -7,4 +7,8 @@ class SearchFrame(tk.Frame):
         super().__init__(master)
         self.lbl_title = tk.Label(self, text='Find Patients')
         self.lbl_title.grid(row=0, column=0, sticky='nsew')
+        self.lbl_f_name = tk.Label(self, text='First Name')
+        self.lbl_f_name.grid(row=1, column=0, sticky='e')
+        self.ent_f_name = tk.Entry(self)
+        self.ent_f_name.grid(row=1, column=1, sticky='we')
         self.grid(row=0, column=1, sticky='nsew')
