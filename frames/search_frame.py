1e560a282337345c3e12db4ddde7e3c0f600065a
adam <adam.lepkowski@hotmail.com>
2021-12-07 13:46:54 +0100

    Add title label

--- frames/search_frame.py
+++ frames/search_frame.py
@@ -5,4 +5,5 @@ class SearchFrame(tk.Frame):
 
     def __init__(self):
         super().__init__()
-        
+        self.lbl_title = tk.Label(self, text='Find Patients')
+        self.lbl_title.grid(row=0, column=0, sticky='nsew')
