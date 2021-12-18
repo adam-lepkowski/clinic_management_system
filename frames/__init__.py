34f6e56a98c3802f3d3ab5ea398f79f164148b10
adam <adam.lepkowski@hotmail.com>
2021-12-18 22:23:50 +0100

    Import PatientFrame

--- frames/__init__.py
+++ frames/__init__.py
@@ -1,3 +1,4 @@
 from frames.registration_frame import RegistrationFrame
+from frames.patient_frame import PatientFrame
 from frames.title_screen import TitleScreen
 from frames.search_frame import SearchFrame
