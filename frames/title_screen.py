2bbf4c952f07140e653114d175d5827db13d9198
adam <adam.lepkowski@hotmail.com>
2021-12-27 19:38:16 +0100

    Refactor path handling

--- frames/title_screen.py
+++ frames/title_screen.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from pathlib import Path
 
 
 class TitleScreen(tk.Frame):
@@ -11,8 +12,10 @@ class TitleScreen(tk.Frame):
         self.columnconfigure(0, weight=1)
         self.lbl_title = tk.Label(self, text='Clinic Management System')
         self.lbl_title.grid(row=0, column=0, sticky='we', pady=10)
-        self.img_reg = tk.PhotoImage(file=r'images\resized\register.png')
-        self.img_find = tk.PhotoImage(file=r'images\resized\find.png')
+        register = Path('images/resized/register.png')
+        find = Path('images/resized/find.png')
+        self.img_reg = tk.PhotoImage(file=register)
+        self.img_find = tk.PhotoImage(file=find)
         self.frm_buttons = tk.Frame(self)
         self.frm_buttons.grid(row=1, column=0, sticky='nsew')
         self.btn_reg = tk.Button(
