c0ee2142e150153196e795b874aaab0b4f34b72b
adam <adam.lepkowski@hotmail.com>
2021-12-29 22:24:23 +0100

    Add appointment button

--- frames/title_screen.py
+++ frames/title_screen.py
@@ -14,8 +14,10 @@ class TitleScreen(tk.Frame):
         self.lbl_title.grid(row=0, column=0, sticky='we', pady=10)
         register = Path('images/resized/register.png')
         find = Path('images/resized/find.png')
+        appointment = Path('images/resized/appointment.png')
         self.img_reg = tk.PhotoImage(file=register)
         self.img_find = tk.PhotoImage(file=find)
+        self.img_appointment = tk.PhotoImage(file=appointment)
         self.frm_buttons = tk.Frame(self)
         self.frm_buttons.grid(row=1, column=0, sticky='nsew')
         self.btn_reg = tk.Button(
@@ -26,4 +28,9 @@ class TitleScreen(tk.Frame):
             self.frm_buttons, text='Find', image=self.img_find,
             compound=tk.TOP, command=lambda: master.change_frame(2))
         self.btn_find.grid(row=0, column=1)
+        self.btn_appointment = tk.Button(
+            self.frm_buttons, text='Appointment', image=self.img_appointment,
+            compound=tk.TOP
+        )
+        self.btn_appointment.grid(row=0, column=2)
         self.grid(row=0, column=1, sticky='nsew')
