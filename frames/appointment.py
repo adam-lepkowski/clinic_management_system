b2e98d5d1c8226ba7303e569d98a45230a1e42bf
adam <adam.lepkowski@hotmail.com>
2021-12-30 20:30:31 +0100

    Add dateentry

--- frames/appointment.py
+++ frames/appointment.py
@@ -1,7 +1,12 @@
 import tkinter as tk
 
+from tkcalendar import DateEntry
+
 
 class Appointment(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
+        self.ent_date = DateEntry(self, date_pattern='y-mm-dd')
+        self.ent_date.grid(row=0, column=0)
+        self.grid(row=0, column=1)
