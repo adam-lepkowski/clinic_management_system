d8d14fa4625b87e02718ce8167f46fdde606a9ab
adam <adam.lepkowski@hotmail.com>
2021-12-30 20:33:47 +0100

    Add title and change widget placement

--- frames/appointment.py
+++ frames/appointment.py
@@ -7,6 +7,8 @@ class Appointment(tk.Frame):
 
     def __init__(self, master):
         super().__init__(master)
+        self.lbl_title = tk.Label(self, text='Appointment Schedule')
+        self.lbl_title.grid(row=0, column=0, sticky='nsew')
         self.ent_date = DateEntry(self, date_pattern='y-mm-dd')
-        self.ent_date.grid(row=0, column=0)
-        self.grid(row=0, column=1)
+        self.ent_date.grid(row=1, column=0)
+        self.grid(row=0, column=1, sticky='nsew')
