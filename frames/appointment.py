f7d2c08b44ed6a7f3bbe2cd85cae7abe2fd0e922
adam <adam.lepkowski@hotmail.com>
2022-01-01 17:10:16 +0100

    Refactor specialties and optionmenus
    Make optionmenus use the same dictionary for values

--- frames/appointment.py
+++ frames/appointment.py
@@ -13,19 +13,22 @@ class Appointment(tk.Frame):
         self.ent_date.grid(row=1, column=0)
         self.lbl_specialty = tk.Label(self, text='Specialty')
         self.lbl_specialty.grid(row=1, column=1, sticky='we')
-        specialties = ['Dermatology', 'Ophthalmology']
+        self.specialties = {
+            'All': ['Der_1', 'Der_2', 'Oph_1', 'Oph_2'],
+            'Dermatology': ['Der_1', 'Der_2'],
+            'Ophthalmology': ['Oph_1', 'Oph_2']
+        }
         self.var_specialty = tk.StringVar(self)
         self.opt_specialty = tk.OptionMenu(
-            self, self.var_specialty, *specialties
+            self, self.var_specialty, *list(self.specialties.keys())
         )
         self.opt_specialty.grid(row=1, column=2, sticky='we')
         self.lbl_doctor = tk.Label(self, text='Doctor')
         self.lbl_doctor.grid(row=1, column=3, sticky='we')
-        doctors = [
-            'Dermatologist 1', 'Dermatologist 2', 'Ophthalmologist 1',
-            'Ophthalmologist 2'
-        ]
+        # todo użyć variable trace, zeby śledzić zmiany w stringvar
         self.var_doctor = tk.StringVar(self)
-        self.opt_doctor = tk.OptionMenu(self, self.var_doctor, *doctors)
+        self.opt_doctor = tk.OptionMenu(
+            self, self.var_doctor, *self.specialties['All']
+        )
         self.opt_doctor.grid(row=1, column=4, sticky='we')
         self.grid(row=0, column=1, sticky='nsew')
