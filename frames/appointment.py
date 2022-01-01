62e76592935aaa84adf790b502ec9fcd5219e6c6
adam <adam.lepkowski@hotmail.com>
2022-01-01 19:44:51 +0100

    Centre title label

--- frames/appointment.py
+++ frames/appointment.py
@@ -8,7 +8,9 @@ class Appointment(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.lbl_title = tk.Label(self, text='Appointment Schedule')
-        self.lbl_title.grid(row=0, column=0, sticky='nsew', pady=10)
+        self.lbl_title.grid(
+            row=0, column=0, sticky='nsew', pady=10, columnspan=5
+        )
         self.ent_date = DateEntry(self, date_pattern='y-mm-dd')
         self.ent_date.grid(row=1, column=0)
         self.lbl_specialty = tk.Label(self, text='Specialty')
