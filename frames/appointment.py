a859df14b8788f9860080909fef4219499cd1da7
adam <adam.lepkowski@hotmail.com>
2022-01-09 18:41:44 +0100

    Refactor confirm_appointment

--- frames/appointment.py
+++ frames/appointment.py
@@ -31,10 +31,13 @@ class Appointment(tk.Toplevel):
 
     def confirm_appointment(self):
         document = self.ent_pat.get()
-        patient = self.db.find_patient(document_no=document)
-        if patient:
+        patient = self.db.find_patient(document_no=document)[0]
+        doctor = self.var_spec.get()
+        if patient and doctor:
             title = 'Appointment scheduled'
-            message = f'Patient {patient[0]} scheduled for an appointment'
+            message = f'Patient scheduled for an appointment'
+            patient_id = patient[0]
+            self.db.register_appointment(patient_id, self.date, doctor)
             msg.showinfo(title=title, message=message)
         else:
             msg.showinfo('Patient not found', 'Patient not found')
