5452811aff976d9509c2e535566dd981f28c98e9
adam <adam.lepkowski@hotmail.com>
2022-01-14 20:22:10 +0100

    Refactor test

--- tests/test_db.py
+++ tests/test_db.py
@@ -12,7 +12,7 @@ class TestDBInit(unittest.TestCase):
     def test_init(self):
         db = DB(':memory:')
         sql = """SELECT name FROM sqlite_master WHERE type='table'"""
-        expected = [('patient', ), ('appointment', )]
+        expected = [('patient', ), ('appointment', ), ('doctor', )]
         result = db.cur.execute(sql).fetchall()
         self.assertEqual(expected, result)
 
