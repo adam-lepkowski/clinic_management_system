628312b9f528d68f03a00059cb8edcbae15b1c54
adam <adam.lepkowski@hotmail.com>
2022-02-28 21:30:44 +0100

    Add Admin to frames

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 from db import DB
 from frames import (Registration, TitleScreen, Search, Schedule,
-                    FirstLaunchScreen, Login)
-from frames.const import TITLE_SCRN, REGISTRATION, SEARCH, SCHEDULE
+                    FirstLaunchScreen, Login, AdminPanel)
+from frames.const import TITLE_SCRN, REGISTRATION, SEARCH, SCHEDULE, ADMIN
 
 
 class ClinicManagementSystem(tk.Tk):
@@ -56,7 +56,8 @@ class ClinicManagementSystem(tk.Tk):
             TITLE_SCRN: TitleScreen(self),
             REGISTRATION: Registration(self),
             SEARCH: Search(self),
-            SCHEDULE: Schedule(self)
+            SCHEDULE: Schedule(self),
+            ADMIN: AdminPanel(self)
         }
         self.change_frame(TITLE_SCRN)
 
