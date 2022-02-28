7d0550979d264f5181a0372d83e794ed4ae0a642
adam <adam.lepkowski@hotmail.com>
2022-02-28 21:18:48 +0100

    Add AdminPanel

---
+++ frames/admin_panel.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter.ttk import Notebook
+
+from const import APP_FRAMES_GRID
+
+class AdminPanel(Notebook):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.frm_add_emp = tk.Frame(self)
+        self.frm_create_usr = tk.Frame(self)
+        self.add(self.frm_add_emp, text='Add Employee')
+        self.add(self.frm_create_usr, text='Create User Account')
+        self.grid(APP_FRAMES_GRID)
