1ce29b5c7062279db0c4e83b0c6070fdfdf8925f
adam <adam.lepkowski@hotmail.com>
2022-02-28 21:43:28 +0100

    Refactor AdminPanel

--- frames/admin_panel.py
+++ frames/admin_panel.py
@@ -34,8 +34,8 @@ class AdminPanel(Notebook):
 
     def __init__(self, master):
         super().__init__(master)
-        self.frm_add_emp = tk.Frame(self)
+        self.frm_emp = Employee(self)
         self.frm_create_usr = tk.Frame(self)
-        self.add(self.frm_add_emp, text='Add Employee')
+        self.add(self.frm_emp, text='Add Employee')
         self.add(self.frm_create_usr, text='Create User Account')
         self.grid(APP_FRAMES_GRID)
