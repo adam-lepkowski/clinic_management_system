9b032e5b9738a49ddbb11cacb33bde3a0fcf15cd
adam <adam.lepkowski@hotmail.com>
2022-02-28 21:28:53 +0100

    Import AdminPanel

--- frames/__init__.py
+++ frames/__init__.py
@@ -6,3 +6,4 @@ from frames.medical_record import MedicalRecord
 from frames.title_screen import TitleScreen
 from frames.search_frame import Search
 from frames.appointment import Schedule
+from frames.admin_panel import AdminPanel
