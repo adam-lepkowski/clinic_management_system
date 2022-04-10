451382122883f127a870214161200341ee59ad5c
adam <adam.lepkowski@hotmail.com>
2022-04-10 22:01:31 +0200

    Remove get appointment

--- frames/appointment.py
+++ frames/appointment.py
@@ -367,28 +367,3 @@ class Schedule(tk.Frame):
                     self.opt_doctor.configure(state='disabled')
                     self.opt_specialty.configure(state='disabled')
                     break
-
-    def get_appointment(self, doc_id, app_datetime):
-        """
-        Get scheduled appointment
-
-        Parameters
-        ---------------
-        doc_id : int or str
-            doctor's id
-        app_datetime : datetime or str
-            datetime in format YYYY-MM-DD HH:MM
-
-        Returns
-        ---------------
-        dictionary
-        OR
-        empty list
-        """
-
-        appointment = self.master.db.find(
-            'appointment', doctor_id=doc_id, app_datetime=app_datetime
-        )
-        if appointment:
-            appointment = self.master.db.row_to_dict('appointment', appointment[0])
-        return appointment
